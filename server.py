1f170003e9aa109fa569ac0f2ffe2f569d8aeeb1
winstxnhdw <winstxnhdw@gmail.com>
2021-08-14 23:20:38 +0800

    test

--- server.py
+++ server.py
@@ -6,12 +6,9 @@ from libs.translate_tweet import translate_tweet
 class TranslateTweet(Flask):
 
   def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
-
-    if not self.debug or os.getenv('WERKZEUG_RUN_MAIN') == 'true':
-      with self.app_context():
-        translate_tweet()
-
+      
     super(TranslateTweet, self).run(host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
+    translate_tweet()
 
 if __name__ == "__main__":
     app = TranslateTweet(__name__)
