fe82f47aa7ba8453cfc6088c7bfb2cbe1261ffd5
winstxnhdw <winstxnhdw@gmail.com>
2021-08-14 19:41:31 +0800

    fix?

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     translate_tweet()
+    return " "
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(env.get('PORT')), debug=False)
